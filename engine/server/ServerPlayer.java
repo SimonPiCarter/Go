40a30d857cebd729f1d64d5eaee8161b2d86618e
simon.pierre <sh.pierre@laposte.net>
2016-04-02 15:06:27 +0200

    Added : message with ip info
    Added : param for server in ClientMain

--- engine/server/ServerPlayer.java
+++ engine/server/ServerPlayer.java
@@ -6,9 +6,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 
+import server.tools.IPAdressToolbox;
+
 public class ServerPlayer extends AbstractOnlinePlayer {
 	
 	public ServerPlayer() throws IOException {
+		System.out.println("Your local IP: "+IPAdressToolbox.getLocalIP());
+		System.out.println("Your global IP: "+IPAdressToolbox.getGlobalIP());
+		System.out.println("Waiting for client to connect on port 9090...");
 		ServerSocket listener = new ServerSocket(9090);
 		socket = listener.accept();
 		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
